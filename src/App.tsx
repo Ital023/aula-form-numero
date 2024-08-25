16f89f2518fcc038b6d1f0c525ff993e3afec20a
Ital023 <contadoitalo911@gmail.com>
2024-08-25 12:32:02 -0300

    feat: finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+
+type FormData = {
+  salary?: number
+}
+
 export default function App() {
+  
+  const[formData, setFormData] = useState<FormData>({});
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleInputChange(event: any){
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setFormData({...formData, [name]: value});
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    console.log(formData.salary || 0);
+  }
+  
   return (
-  <h1>oi</h1>
-     
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+      onChange={handleInputChange} 
+      name="salary"
+      value={formData.salary || ""}
+      placeholder="Digite um numero" />
+      
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
